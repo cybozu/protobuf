26a5c0a6d7bf5011a3e678de03a2e59e86dc4d70
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-04-10 02:27:14 +0900

    First step for plugin

--- cmd/protoc-gen-es-cybozu-validate/src/typescript.ts
+++ cmd/protoc-gen-es-cybozu-validate/src/typescript.ts
@@ -1,5 +1,12 @@
 import type { Schema } from "@bufbuild/protoplugin/ecmascript";
 
 export function generateTs(schema: Schema) {
-  // TODO: implement
+  for (const file of schema.files) {
+    if (file.messages.length === 0) {
+      // noting to generate
+      continue;
+    }
+    const filename = `${file.name}_cybozu_validate.pb.ts`;
+    const f = schema.generateFile(filename);
+  }
 }
