e2d40ffc55fd0547d27984d2c16d293f10101450
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-04-10 04:11:33 +0900

    Add bin

--- cmd/protoc-gen-es-cybozu-validate/src/typescript.ts
+++ cmd/protoc-gen-es-cybozu-validate/src/typescript.ts
@@ -1,10 +1,11 @@
 import type { Schema } from "@bufbuild/protoplugin/ecmascript";
-import { localName } from "@bufbuild/protobuf/dist/types/private/names";
+import { localName } from "@bufbuild/protoplugin/ecmascript";
 
 function capitalizeFirstLetter(value: string): string {
   return value.charAt(0).toUpperCase() + value.slice(1);
 }
 
+// prettier-ignore
 export function generateTs(schema: Schema) {
   for (const file of schema.files) {
     if (file.messages.length === 0) {
@@ -24,8 +25,8 @@ export function generateTs(schema: Schema) {
       for (const field of message.fields) {
         const localFieldName = capitalizeFirstLetter(localName(field));
         const capitalizedFieldName = capitalizeFirstLetter(localFieldName);
-        f.print`validate${capitalizedFieldName}(value: unkown): asserts value is ${MessageAsType}[${localFieldName}] {`;
-        f.print`},`;
+        f.print`  validate${capitalizedFieldName}(value: unknown): asserts value is ${MessageAsType}[${localFieldName}] {`;
+        f.print`  },`;
       }
 
       f.print`}`;
