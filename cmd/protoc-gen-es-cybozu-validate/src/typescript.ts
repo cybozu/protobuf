1d055cc5a86db6ad50e8f51fc2cfb799456feab4
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-04-10 02:59:01 +0900

    Temporary generate empty field validation method

--- cmd/protoc-gen-es-cybozu-validate/src/typescript.ts
+++ cmd/protoc-gen-es-cybozu-validate/src/typescript.ts
@@ -1,4 +1,9 @@
 import type { Schema } from "@bufbuild/protoplugin/ecmascript";
+import { localName } from "@bufbuild/protobuf/dist/types/private/names";
+
+function capitalizeFirstLetter(value: string): string {
+  return value.charAt(0).toUpperCase() + value.slice(1);
+}
 
 export function generateTs(schema: Schema) {
   for (const file of schema.files) {
@@ -8,5 +13,22 @@ export function generateTs(schema: Schema) {
     }
     const filename = `${file.name}_cybozu_validate.pb.ts`;
     const f = schema.generateFile(filename);
+
+    const { Message } = schema.runtime;
+    const MessageAsType = Message.toTypeOnly();
+
+    for (const message of file.messages) {
+      const localMessageName = localName(message);
+      f.print`export const ${localMessageName}Validators = {`;
+
+      for (const field of message.fields) {
+        const localFieldName = capitalizeFirstLetter(localName(field));
+        const capitalizedFieldName = capitalizeFirstLetter(localFieldName);
+        f.print`validate${capitalizedFieldName}(value: unkown): asserts value is ${MessageAsType}[${localFieldName}] {`;
+        f.print`},`;
+      }
+
+      f.print`}`;
+    }
   }
 }
