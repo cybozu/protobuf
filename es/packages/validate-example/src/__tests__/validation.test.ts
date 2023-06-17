a4e5b4edbdbec8eafdcab4533f314cdf8663c4d6
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-06-17 17:03:57 +0900

    Add tests for `OptionalScalarsValidators`

--- es/packages/validate-example/src/__tests__/validation.test.ts
+++ es/packages/validate-example/src/__tests__/validation.test.ts
@@ -1,7 +1,10 @@
 import { describe, it } from "node:test";
 import assert from "node:assert/strict";
 
-import { ScalarsValidators } from "../gen/validation_cybozu_validate.pb";
+import {
+  ScalarsValidators,
+  OptionalScalarsValidators,
+} from "../gen/validation_cybozu_validate.pb";
 
 describe("validation", () => {
   describe("ScalarsValidators", () => {
@@ -279,6 +282,368 @@ describe("validation", () => {
       it("does not throws error when receive byteLength <= 10", () => {
         assert.doesNotThrow(() => {
           ScalarsValidators.validateBytes(new Uint8Array(10));
+        });
+      });
+    });
+  });
+
+  describe("OptionalScalarsValidators", () => {
+    describe("validateFloat", () => {
+      it("does not throw an error when value is null", () => {
+        assert.doesNotThrow(() => {
+          OptionalScalarsValidators.validateFloat(null);
+        });
+      });
+      it("throws an error when receive non number value", () => {
+        assert.throws(() => {
+          OptionalScalarsValidators.validateFloat("foo");
+        });
+      });
+      it("throws an error when recieve the value >= 3.2", () => {
+        assert.throws(() => {
+          OptionalScalarsValidators.validateFloat(3.3);
+        });
+      });
+      it("does not throws error when receive the value < 3.2", () => {
+        assert.doesNotThrow(() => {
+          OptionalScalarsValidators.validateFloat(3.1);
+        });
+      });
+    });
+
+    describe("validateDouble", () => {
+      it("does not throw an error when value is null", () => {
+        assert.doesNotThrow(() => {
+          OptionalScalarsValidators.validateDouble(null);
+        });
+      });
+
+      it("throws an error when receive non number value", () => {
+        assert.throws(() => {
+          OptionalScalarsValidators.validateDouble("foo");
+        });
+      });
+
+      it("throws an error when recieve the value <= 3.2", () => {
+        assert.throws(() => {
+          OptionalScalarsValidators.validateDouble(3.1);
+        });
+      });
+
+      it("does not throws error when receive the value > 3.2", () => {
+        assert.doesNotThrow(() => {
+          OptionalScalarsValidators.validateDouble(3.3);
+        });
+      });
+    });
+
+    describe("validateInt32", () => {
+      it("does not throw an error when value is null", () => {
+        assert.doesNotThrow(() => {
+          OptionalScalarsValidators.validateInt32(null);
+        });
+      });
+
+      it("throws an error when receive non number value", () => {
+        assert.throws(() => {
+          OptionalScalarsValidators.validateInt32("foo");
+        });
+      });
+
+      it("throws an error when recieve the value > -3", () => {
+        assert.throws(() => {
+          OptionalScalarsValidators.validateInt32(-2);
+        });
+      });
+
+      it("does not throws error when receive the value <= -3", () => {
+        assert.doesNotThrow(() => {
+          OptionalScalarsValidators.validateInt32(-3);
+        });
+      });
+    });
+
+    describe("validateInt64", () => {
+      it("does not throw an error when value is null", () => {
+        assert.doesNotThrow(() => {
+          OptionalScalarsValidators.validateInt64(null);
+        });
+      });
+
+      it("throws an error when receive non number value", () => {
+        assert.throws(() => {
+          OptionalScalarsValidators.validateInt64("foo");
+        });
+      });
+
+      it("throws an error when recieve the value < 1", () => {
+        assert.throws(() => {
+          OptionalScalarsValidators.validateInt64(0);
+        });
+      });
+
+      it("does not throws error when receive the value >= 1", () => {
+        assert.doesNotThrow(() => {
+          OptionalScalarsValidators.validateInt64(1);
+        });
+      });
+    });
+
+    describe("validateUint32", () => {
+      it("does not throw an error when value is null", () => {
+        assert.doesNotThrow(() => {
+          OptionalScalarsValidators.validateUint32(null);
+        });
+      });
+
+      it("throws an error when receive non number value", () => {
+        assert.throws(() => {
+          OptionalScalarsValidators.validateUint32("foo");
+        });
+      });
+
+      it("throws an error when recieve the value <= 1 or value >= 5", () => {
+        assert.throws(() => {
+          OptionalScalarsValidators.validateUint32(1);
+        });
+        assert.throws(() => {
+          OptionalScalarsValidators.validateUint32(5);
+        });
+      });
+
+      it("does not throws error when receive the value between 1 and 5", () => {
+        assert.doesNotThrow(() => {
+          OptionalScalarsValidators.validateUint32(3);
+        });
+      });
+    });
+
+    describe("validateUint64", () => {
+      it("does not throw an error when value is null", () => {
+        assert.doesNotThrow(() => {
+          OptionalScalarsValidators.validateUint64(null);
+        });
+      });
+
+      it("throws an error when receive non number value", () => {
+        assert.throws(() => {
+          OptionalScalarsValidators.validateUint64("foo");
+        });
+      });
+
+      it("throws an error when recieve the value < 1 or value > 5", () => {
+        assert.throws(() => {
+          OptionalScalarsValidators.validateUint64(0);
+        });
+        assert.throws(() => {
+          OptionalScalarsValidators.validateUint64(6);
+        });
+      });
+
+      it("does not throws error when receive the value between 1 and 5", () => {
+        assert.doesNotThrow(() => {
+          OptionalScalarsValidators.validateUint64(3);
+        });
+      });
+    });
+
+    describe("validateSint32", () => {
+      it("does not throw an error when value is null", () => {
+        assert.doesNotThrow(() => {
+          OptionalScalarsValidators.validateSint32(null);
+        });
+      });
+
+      it("throws an error when receive non number value", () => {
+        assert.throws(() => {
+          OptionalScalarsValidators.validateSint32("foo");
+        });
+      });
+
+      it("throws an error when recieve the value <= 1 or value >= 5", () => {
+        assert.throws(() => {
+          OptionalScalarsValidators.validateSint32(1);
+        });
+        assert.throws(() => {
+          OptionalScalarsValidators.validateSint32(5);
+        });
+      });
+
+      it("does not throws error when receive the value between 1 and 5", () => {
+        assert.doesNotThrow(() => {
+          OptionalScalarsValidators.validateSint32(3);
+        });
+      });
+    });
+
+    describe("validateSint64", () => {
+      it("does not throw an error when value is null", () => {
+        assert.doesNotThrow(() => {
+          OptionalScalarsValidators.validateSint64(null);
+        });
+      });
+
+      it("throws an error when receive non number value", () => {
+        assert.throws(() => {
+          OptionalScalarsValidators.validateSint64("foo");
+        });
+      });
+
+      it("throws an error when recieve the value < 1 or value > 5", () => {
+        assert.throws(() => {
+          OptionalScalarsValidators.validateSint64(0);
+        });
+        assert.throws(() => {
+          OptionalScalarsValidators.validateSint64(6);
+        });
+      });
+
+      it("does not throws error when receive the value between 1 and 5", () => {
+        assert.doesNotThrow(() => {
+          OptionalScalarsValidators.validateSint64(3);
+        });
+      });
+    });
+
+    describe("validateFixed32", () => {
+      it("does not throw an error when value is null", () => {
+        assert.doesNotThrow(() => {
+          OptionalScalarsValidators.validateFixed32(null);
+        });
+      });
+
+      it("throws an error when receive non number value", () => {
+        assert.throws(() => {
+          OptionalScalarsValidators.validateFixed32("foo");
+        });
+      });
+
+      it("throws an error when recieve the value <= 1 or value >= 5", () => {
+        assert.throws(() => {
+          OptionalScalarsValidators.validateFixed32(1);
+        });
+        assert.throws(() => {
+          OptionalScalarsValidators.validateFixed32(5);
+        });
+      });
+
+      it("does not throws error when receive the value between 1 and 5", () => {
+        assert.doesNotThrow(() => {
+          OptionalScalarsValidators.validateFixed32(3);
+        });
+      });
+    });
+
+    describe("validateFixed64", () => {
+      it("does not throw an error when value is null", () => {
+        assert.doesNotThrow(() => {
+          OptionalScalarsValidators.validateFixed64(null);
+        });
+      });
+
+      it("throws an error when receive non number value", () => {
+        assert.throws(() => {
+          OptionalScalarsValidators.validateFixed64("foo");
+        });
+      });
+
+      it("throws an error when recieve the value < 1 or value > 5", () => {
+        assert.throws(() => {
+          OptionalScalarsValidators.validateFixed64(0);
+        });
+        assert.throws(() => {
+          OptionalScalarsValidators.validateFixed64(6);
+        });
+      });
+
+      it("does not throws error when receive the value between 1 and 5", () => {
+        assert.doesNotThrow(() => {
+          OptionalScalarsValidators.validateFixed64(3);
+        });
+      });
+    });
+
+    describe("validateSfixed32", () => {
+      it("does not throw an error when value is null", () => {
+        assert.doesNotThrow(() => {
+          OptionalScalarsValidators.validateSfixed32(null);
+        });
+      });
+
+      it("throws an error when receive non number value", () => {
+        assert.throws(() => {
+          OptionalScalarsValidators.validateSfixed32("foo");
+        });
+      });
+
+      it("throws an error when recieve the value <= 1 or value >= 5", () => {
+        assert.throws(() => {
+          OptionalScalarsValidators.validateSfixed32(1);
+        });
+        assert.throws(() => {
+          OptionalScalarsValidators.validateSfixed32(5);
+        });
+      });
+
+      it("does not throws error when receive the value between 1 and 5", () => {
+        assert.doesNotThrow(() => {
+          OptionalScalarsValidators.validateSfixed32(3);
+        });
+      });
+    });
+
+    describe("validateSfixed64", () => {
+      it("does not throw an error when value is null", () => {
+        assert.doesNotThrow(() => {
+          OptionalScalarsValidators.validateSfixed64(null);
+        });
+      });
+
+      it("throws an error when receive non number value", () => {
+        assert.throws(() => {
+          OptionalScalarsValidators.validateSfixed64("foo");
+        });
+      });
+
+      it("throws an error when recieve the value < 1 or value > 5", () => {
+        assert.throws(() => {
+          OptionalScalarsValidators.validateSfixed64(0);
+        });
+        assert.throws(() => {
+          OptionalScalarsValidators.validateSfixed64(6);
+        });
+      });
+
+      it("does not throws error when receive the value between 1 and 5", () => {
+        assert.doesNotThrow(() => {
+          OptionalScalarsValidators.validateSfixed64(3);
+        });
+      });
+    });
+
+    describe.skip("validateString", () => {
+      // TODO: add tests for validateString
+    });
+
+    describe("validateBytes", () => {
+      it("does not throw an error when value is null", () => {
+        assert.doesNotThrow(() => {
+          OptionalScalarsValidators.validateBytes(null);
+        });
+      });
+      it("throws an error when receive non Uint8Array value", () => {
+        assert.throws(() => {
+          OptionalScalarsValidators.validateBytes("foo");
+        });
+      });
+      it("throws an error when recieve byteLength > 10", () => {
+        assert.throws(() => {
+          OptionalScalarsValidators.validateBytes(new Uint8Array(11));
+        });
+      });
+      it("does not throws error when receive byteLength <= 10", () => {
+        assert.doesNotThrow(() => {
+          OptionalScalarsValidators.validateBytes(new Uint8Array(10));
         });
       });
     });
